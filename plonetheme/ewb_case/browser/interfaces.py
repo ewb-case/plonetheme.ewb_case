4542f0079da80f7ac2a452d5d42c45bc62ef4c12
Matt Bierner <mattbierner@gmail.com>
2012-01-04 23:05:24 -0600

    Initial setup for two sides of tabs

--- plonetheme/ewb_case/browser/interfaces.py
+++ plonetheme/ewb_case/browser/interfaces.py
@@ -1,5 +1,9 @@
 from zope.interface import Interface
+
 from plone.theme.interfaces import IDefaultPloneLayer
+
+from zope.viewlet.interfaces import IViewletManager
+
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope browser layer.
@@ -11,3 +15,7 @@ class IThemeView(Interface):
     def getColumnsClass():
         """ Returns the CSS class based on columns presence. """
 
+
+
+class IPortalTabs(IViewletManager):
+    pass
