e2b51b4b02a6b329efc1418458448dce1c5dfe4b
Matt Bierner <mattbierner@gmail.com>
2012-01-22 19:08:45 -0500

    Added info to setup.py

--- plonetheme/ewb_case/__init__.py
+++ plonetheme/ewb_case/__init__.py
@@ -1,10 +1,9 @@
-from Products.CMFCore.DirectoryView import registerFileExtension
-from Products.CMFCore.FSFile import FSFile
-
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
     
     # Register SVG File Extensions for CMF
-     
+    from Products.CMFCore.DirectoryView import registerFileExtension
+    from Products.CMFCore.FSFile import FSFile
+
     registerFileExtension('svg', FSFile)
--- setup.py
+++ setup.py
@@ -4,34 +4,35 @@ import os
 version = '0.1.0'
 
 setup(name='plonetheme.ewb_case',
-      version=version,
-      description="A Plone 4 theme for EWB Case",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      classifiers=[
+    version=version,
+    description="A Plone 4 theme for EWB Case",
+    long_description=open("README.txt").read() + "\n" +
+                     open(os.path.join("docs", "HISTORY.txt")).read(),
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
-        ],
-      keywords='',
-      author='Matt Bierner',
-      author_email='mattbierner@gmail.com',
-      url='https://github.com/mattbierner/plonetheme.ewb_case',
-      license='Creative Commons Attribution 3.0 Unported',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plonetheme'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.registry',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      setup_requires=[],
-      )
-
+    ],
+    keywords='',
+    author='Matt Bierner',
+    author_email='mattbierner@gmail.com',
+    url='https://github.com/mattbierner/plonetheme.ewb_case',
+    license='Creative Commons Attribution 3.0 Unported',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plonetheme'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Plone>=4.0',
+        'plone.app.registry',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    setup_requires=[],
+)
+    
